f402e87f425bfddc0a58bd163acdd6f6993d2d45
evan-beep <evan.hehehe@gmail.com>
2024-05-04 16:53:09 +0800

    db update and fix for INFO

--- retrieve_and_save_data.py
+++ retrieve_and_save_data.py
@@ -36,6 +36,7 @@ def main():
         get_OCG_card_prices(set_code)
         get_TCG_card_prices(details['set_names'][0])
         
+        get_OCG_card_prices('INFO') #patch, not perma fix
         time.sleep(0.1)
         
     get_rakuten()
