bcedfc53c699afcdaa4e3de44b56fe57ec24b561
Evan6684 <evan.hehehe@gmail.com>
2024-05-31 10:48:57 -0400

    db update

---
+++ check.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+# Connect to the SQLite database
+conn = sqlite3.connect('Card_Prices.db')
+cursor = conn.cursor()
+
+# List of tables
+tables = ['ocg_prices', 'rakuten_prices', 'tcg_prices']
+
+# Dictionary to store unique DateTime values
+unique_datetime_values = {}
+
+for table in tables:
+    # Query to get unique DateTime values
+    query = f"SELECT DISTINCT DateTime FROM {table}"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    unique_datetime_values[table] = [row[0] for row in result]
+
+# Close the connection
+conn.close()
+
+# Display the results
+for table, datetimes in unique_datetime_values.items():
+    print(f"Unique DateTime values in {table}:")
+    for dt in datetimes:
+        print(dt)
+    print()  # For better readability
--- retrieve_and_save_data.py
+++ retrieve_and_save_data.py
@@ -6,40 +6,46 @@ import json
 import datetime
 import time
 
+
 def load_json_data(file_path):
     with open(file_path, 'r') as file:
         data = json.load(file)
     return data
+
 
 def filter_sets(data):
     filtered_sets = {}
     for set_code, details in data.items():
         try:
             year = int(details['tcg_date'].split('-')[0])
-            card_count = int(details.get('card_count', 0))  # Safe default if card_count is missing
+            # Safe default if card_count is missing
+            card_count = int(details.get('card_count', 0))
             if year > 2022 and card_count == 101:
                 filtered_sets[set_code] = details
         except ValueError:
             continue
     return filtered_sets
 
+
 def main():
-    
-    #update sets every week
-    #if datetime.datetime.now().weekday() == 3:
+
+    # update sets every week
+    # if datetime.datetime.now().weekday() == 3:
     #    get_all_sets()
-        
-    data = load_json_data('sets\\code_to_name.json')
+
+    data = load_json_data('./sets/code_to_name.json')
     filtered_sets = filter_sets(data)
-    
+
     for set_code, details in filtered_sets.items():
         get_OCG_card_prices(set_code)
         get_TCG_card_prices(details['set_names'][0])
-        
-        get_OCG_card_prices('INFO') #patch, not perma fix
+
+        get_OCG_card_prices('INFO')  # patch, not perma fix
+        get_OCG_card_prices('dp29')
         time.sleep(0.1)
-        
+
     get_rakuten()
+
 
 if __name__ == "__main__":
     main()
